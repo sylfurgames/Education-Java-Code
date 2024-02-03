65486a7d9ba0e094c39a78ea35032684be3c3d02
Starfall <123963378+sylfurgames@users.noreply.github.com>
2024-02-03 04:51:56 +0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,5 @@
+var siteConfig = {
+  nameSite: "Education Java Code",
+  nameH1: "Education Java Code",
+  version: "0.0.2 beta"
+}
---
+++ main.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function(){
+  
+  //Установка конфига сайта
+  document.getElementById('nameSite').textContent = siteConfig.nameSite;
+  document.getElementById('nameH1').textContent = siteConfig.nameH1;
+  document.getElementById('version').textContent = siteConfig.version;
+
+});
